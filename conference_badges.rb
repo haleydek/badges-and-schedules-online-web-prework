967a89a090f576f7496bbdebe9a34b458390ee19
Haley DeKeyser <hcd09@live.com>
2019-05-11 18:27:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def assign_rooms(speakers)
   speakers.collect do |name|
     room_num = speakers.index(name)
     room_num += 1
-    return "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    "Hello, #{name}! You'll be assigned to room #{room_num}!"
   end
 end
   
