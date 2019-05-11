ced96755737726193a28c46eced4aced9a8c24e5
Haley DeKeyser <hcd09@live.com>
2019-05-11 18:35:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  attendees.each do |name|
+    return puts batch_badge_creator(attendees) + assign_rooms(attendees)
+  end
 end
